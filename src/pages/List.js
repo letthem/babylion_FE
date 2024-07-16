cf252a4f036ec7253ac1fb640d4ae6eb7f49194a
letthem <suyeon3662@naver.com>
2024-07-17 02:45:22 +0900

    Feat: verifyJWT 접근 권한 설정

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Navbar from "../components/Navbar";
 import UserList from "../components/UserList";
+import { useNavigate } from "react-router";
+import { axiosInstance } from "../api/api";
 
 const List = () => {
+  const navigate = useNavigate();
+  const [verify, setVerify] = useState(false);
+
+  const verifyJWT = async () => {
+    try {
+      const response = await axiosInstance.get("/auth/verify", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      });
+      setVerify(true);
+      console.log(response.data);
+    } catch (error) {
+      alert("로그인 ㄱㄱ");
+      navigate("/");
+    }
+  };
+
+  useEffect(() => {
+    verifyJWT();
+  }, []);
+
   return (
     <>
       <Navbar />
-      <ListWrapper>
-        <ListContainer>
-          <UserList />
-        </ListContainer>
-      </ListWrapper>
+      {verify && (
+        <ListWrapper>
+          <ListContainer>
+            <UserList />
+          </ListContainer>
+        </ListWrapper>
+      )}
     </>
   );
 };
